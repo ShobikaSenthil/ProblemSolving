4c16b3be10848871c4d3d726a5b4da277f23ee26
Shobika Senthilkumar <shobika.senthilkumar@cesltd.com>
2022-12-07 22:01:52 +0530

    Updated the changes

--- StarPatternA.js
+++ StarPatternA.js
@@ -1,28 +1,28 @@
 /* Algorithm
 1.Pass the n number and check the input type
 2.If it is not valid,it will print "Invalid input"
-3.Else ,Iterate a for loop from i= 1 to n numbers
-4.Inside the loop 
-    3.1.create a blank string
-    3.2.Create another loop from j = n to greater than or equal to i
-    3.3.After the inner loop,print the string 
-  */
+3.Else , create a blank string and Iterate a for loop from i= 1 to n numbers
+4.Create another loop from j = n to greater than or equal to i
+5.After the inner loop,concat the "\n"
+6.After for loop ,return the string str.
+*/
 
 const n = 5;
 function printStar(n) {
     if (typeof n !== "number") {
-        console.log("Invalid Input");
+        return "Invalid Input";
     }
     else {
+        let str = "";
         for (let i = 1; i <= 5; i++) {
-            let str = "";
             for (let j = n; j >= i; j--) {
                 str += "*";
             }
-            console.log(str);
+            str += "\n";
         }
+        return str;
     }
 }
-printStar(n);
+console.log(printStar(n));
 
 
