5eb01bc4256b870f1bc09df4f4ed4f20915fc432
Shobika Senthilkumar <shobika.senthilkumar@cesltd.com>
2022-12-08 21:42:28 +0530

    Updated kthSmallestLargest.js

--- kthSmallestLargest.js
+++ kthSmallestLargest.js
@@ -1,20 +1,29 @@
 /* Algorithm
 1.Pass the array and k value
-2.Sort the array 
-3.Print array[k-1]
-4.Print array[array.length - 1]
+2.Inside the function,Check whether the k is positive or not .
+3.If k is positive,Sort the array 
+    3.1.Print array[k-1]
+    3.2.Print array[array.length - 1]
+4.Else return Invalid Input
  */
 
-const array = [1, 3, 2, 5, 6, 7, 23, 56, 0];
+const array = [1, 3, 2, 5, 6, 7, 23, 56, 34, 0];
 const k = 5;
 
 function findKthSmallestLargest(array, k) {
-    array.sort(function (a, b) {
-        return a - b;
-    });
-    // console.log(array);
-    console.log("Smallest : ", array[k - 1]);
-    console.log("Largest : ", array[array.length - k]);
+    if (k > 0) {
+        let result_String = "";
+        array.sort(function (a, b) {
+            return a - b;
+        });
+        result_String += "Smallest :" + array[k - 1];
+        result_String += "\nLargest: " + array[array.length - k];
+
+        return result_String;
+    }
+    else {
+        return "Invalid Input";
+    }
 }
 
-findKthSmallestLargest(array, k);
+console.log(findKthSmallestLargest(array, k));
