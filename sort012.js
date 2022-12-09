33cea37a1bb92a1ec296ede0ddbf6dd3afb67869
Shobika Senthilkumar <shobika.senthilkumar@cesltd.com>
2022-12-09 21:55:24 +0530

    Updated sort012.js

--- sort012.js
+++ sort012.js
@@ -1,26 +1,22 @@
 /* 
 1.Pass the array to the function
 2.Inside the function,sort the array
-3.In the for loop,iterate the loop from i=0 to array.length
-4.Iterate the another loop from j = i+1 to array.length
-5.If the array[i] > array[j],then swap the elements
+3.In the for loop,iterate the loop from i=0 to array.length-1
+5.If the array[i] > array[i_+1],then swap the elements and assign i = -1
 6..Return the sorted array
  */
 const array = [0, 2, 1, 2, 0, 1];
 function toSortArray(array) {
-    let temp ;
-    for(let i = 0;i<array.length;i++)
-    {
-        for(let j = i+1;j<array.length;j++)
-        {
-            if(array[i] > array[j])
-            {
-                temp = array[i];
-                array[i]=array[j];
-                array[j]=temp;
-            }
+    let temp;
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+            i = -1;
         }
     }
     return array;
 }
 console.log(toSortArray(array));
+
